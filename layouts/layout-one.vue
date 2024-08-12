9af20cec65afdcd8c562561f074cda2cf1e77675
jakubdvorak <jakub.dvorak@quanti.cz>
2024-08-12 12:43:39 +0200

    first commit

---
+++ layouts/layout-one.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header-one />
+    <main>
+      <slot />
+    </main>
+    <footer-one />
+    <back-to-top />
+    <!-- <modal-product /> -->
+  </div>
+</template>
+
+<style>
+.logo {
+  max-width: 160px;
+}
+</style>
